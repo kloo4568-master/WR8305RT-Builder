f9530b5ad7397efc6e07fd7bb03079971a94e920
kloo4568-master <kloo4568@gmail.com>
2026-07-11 10:05:49 +0800

    Update echo statement for WR8305RT build#!/bin/bash  echo "Using WR8305RT build"

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-echo "Start custom ZBT WR8305RT build"
-
-# USB storage
-echo "CONFIG_PACKAGE_kmod-usb-storage=y" >> .config
-echo "CONFIG_PACKAGE_kmod-fs-ext4=y" >> .config
-echo "CONFIG_PACKAGE_block-mount=y" >> .config
-
-# UPnP
-echo "CONFIG_PACKAGE_luci-app-upnp=y" >> .config
-
-# QoS
-echo "CONFIG_PACKAGE_luci-app-qos=y" >> .config
-echo "CONFIG_PACKAGE_tc=y" >> .config
+echo "Using WR8305RT build"
